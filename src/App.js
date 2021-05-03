547fd8b4e758582fab0b6a3f49a64a93d15b1c2c
Mona <monaaa.saad@yahoo.com>
2021-05-03 21:15:23 +0300

    i

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { ExternalLink } from 'react-external-link';
 import './App.css';
+import Probs from './Probs';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>😊نورتوا الصفحة</h1>
+      <p>تقدرون تتابعونا بوسائل التواصل الاجتماعية، اضغطوا على الروابط ورح تنقلكم إلى صفحاتنا</p>
+        <div style={{display: "flex", justifyContent:"center"}}>
+            <Probs
+                link="https://www.youtube.com/channel/UCIv27uJJUsFPEuEMowjmfsw"
+                img="./youtube.svg"
+            />
+            <Probs
+                link="https://twitter.com/MardodApp"
+                img="./twitter.svg"
+            />
+            <Probs
+                link="https://www.tiktok.com/@mardodapp"
+                img="./tiktok.svg"
+            />
+            <Probs
+                link="https://www.instagram.com/mardodapp"
+                img="./instagram.svg"
+            />
+        </div>
     </div>
   );
 }
